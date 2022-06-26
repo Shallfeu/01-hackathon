5f279578a75f7b8e104f666b3cb248b9bdad8ae3
kirill_korev <kirill.korev.mev@gmail.com>
2022-06-26 12:16:41 +0300

    update app.js with RandomShapeModule

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import './styles.css';
 
 // 1. --- Modules import 
 import { MessageModule } from './modules/message.module';
+import { RandomShapeModule } from './modules/random-shape.module';
 
 // 2. --- Init modules list
 const modules = [
   new MessageModule(),
+  new RandomShapeModule(),
 ];
 
 // Context menu
