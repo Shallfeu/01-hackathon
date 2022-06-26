ddf88d40d1262a65268aea2b2d4bb531133a97d0
Shallfeu <chigvincevra@mail.ru>
2022-06-26 15:41:31 +0300

    start making click

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,11 @@ import { LoggerComponent } from './components/logger.component';
 
 export class ContextMenu extends Menu {
   #modules;
+
   #menuWidthPx;
+
   #menuHeightPx;
+
   #log;
 
   constructor(selector, modules = []) {
@@ -23,42 +26,57 @@ export class ContextMenu extends Menu {
     // Logging
     this.#log.setLog = `Menu open [${top}, ${left}]`;
 
-    return this.el?.style
-      && Object.keys(this.modules).length
-      && (this.el.style = `top:${top || 0}px; left:${left || 0}px; display:block;`);
+    return (
+      this.el?.style &&
+      Object.keys(this.modules).length &&
+      (this.el.style = `top:${top || 0}px; left:${left || 0}px; display:block;`)
+    );
   }
 
   close() {
-    this.#log.setLog = `Menu close`;
+    this.#log.setLog = 'Menu close';
     return this.el?.style && (this.el.style.display = 'none');
   }
 
-  add() {    
-    return Object.keys(this.modules).length
-      && (this.el.innerHTML = Object.values(this.modules).reduce((acc, mod) => {
+  add() {
+    return (
+      Object.keys(this.modules).length &&
+      (this.el.innerHTML = Object.values(this.modules).reduce((acc, mod) => {
         this.#log.setLog = `Add module '${mod.text}' to menu`;
-        return acc += mod.toHTML();        
-    }, ''));
+        return (acc += mod.toHTML());
+      }, ''))
+    );
   }
 
   #computedMuneSizes() {
-    if(this.#menuWidthPx > 0 && this.#menuHeightPx > 0) {
+    if (this.#menuWidthPx > 0 && this.#menuHeightPx > 0) {
       return false;
     }
     const menuClone = this.el.cloneNode(true);
-    menuClone.style.cssText = "display: inline-block;";
+    menuClone.style.cssText = 'display: inline-block;';
     document.body.append(menuClone);
     [this.#menuWidthPx, this.#menuHeightPx] = [menuClone.clientWidth, menuClone.clientHeight];
     menuClone.remove();
 
     this.#log.setLog = `Computed menu sizes [${this.#menuWidthPx} x ${this.#menuHeightPx}]px`;
-  }  
+  }
 
   #computedMenuPosition(topClick, leftClick) {
     return [
       window.innerHeight - topClick > this.#menuHeightPx ? topClick : topClick - this.#menuHeightPx,
-      window.innerWidth - leftClick > this.#menuWidthPx ? leftClick : leftClick - this.#menuWidthPx
+      window.innerWidth - leftClick > this.#menuWidthPx ? leftClick : leftClick - this.#menuWidthPx,
     ];
+  }
+
+  #addSwitchBut() {
+    this.el.insertAdjacentHTML('beforeend', '<li class="switch-style">Ночной режим</li>');
+    this.el
+      .querySelector('.switch-style')
+      .addEventListener('click', (event) =>
+        this.el.classList.contains('dark')
+          ? (event.target.innerHTML = 'Ночной режим') && this.el.classList.remove('dark')
+          : (event.target.innerHTML = 'Дневной режим') && this.el.classList.add('dark'),
+      );
   }
 
   init() {
@@ -66,15 +84,26 @@ export class ContextMenu extends Menu {
     this.add();
 
     // Open menu by click on body
-    document.body.addEventListener('contextmenu', (event) => 
-      this.open(event.offsetY, event.offsetX) && event.preventDefault());
+    document.body.addEventListener(
+      'contextmenu',
+      (event) => this.open(event.offsetY, event.offsetX) && event.preventDefault(),
+    );
 
     // Go to module trigger
-    this.el.addEventListener('click', (event) =>
-      event.target.tagName === 'LI'
-      && this.modules.find((mod) => 
-        (mod.type === event.target.dataset.type) && (this.#log.setLog = `Switch '${mod.text}' on!`))
-        ?.trigger()
-      );
+    this.el.addEventListener(
+      'click',
+      (event) =>
+        event.target.tagName === 'LI' &&
+        this.modules
+          .find(
+            (mod) =>
+              mod.type === event.target.dataset.type &&
+              (this.#log.setLog = `Switch '${mod.text}' on!`),
+          )
+          ?.trigger(),
+    );
+
+    // Add style swith
+    this.#addSwitchBut();
   }
 }
---
+++ src/modules/random-shape.module.js
@@ -0,0 +1,176 @@
+import { Module } from '../core/module';
+import { random } from '../utils';
+import styles from '../css/random-shape.module.css';
+
+export class RandomShapeModule extends Module {
+   #typesOfShape
+  
+   constructor() {
+      super('ShapeModule', 'Случайная фигура');
+      this.#typesOfShape = ['rect', 'circle', 'ellipse', 'polygon', 'line'];
+   }
+
+   randomType() {
+      return random(0, this.#typesOfShape.length - 1);
+   };
+   randomColor() {
+      return `#${(Math.random().toString(16) + '000000').substring(2,8).toUpperCase()}`;
+   };
+   randomGradiend() {
+      return `
+         <defs>
+            <linearGradient id="${random(0, 200)}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+         </defs>
+      `;
+   };
+   createRect() {
+      const gradientID = random(0, 200);
+      const width = random(50, 250);
+      const height = random(50, 250);
+      return `
+        <svg>
+            <defs>
+            <linearGradient id="${gradientID}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+            </defs>
+            <rect 
+            width="${width}" 
+            height="${height}" 
+            fill="url('#${gradientID}')"
+            />
+        </svg>
+      `;
+   };
+   createCircle() {
+      const gradientID = random(0, 200);
+      const width = random(50, 250);
+      const height = random(50, 250);
+      return `
+         <svg width="${width}" height="${height}">
+            <defs>
+            <linearGradient id="${gradientID}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+            </defs>
+            <circle 
+            width="${width}" 
+            height="${height}" 
+            r="${random(20, 100)}" 
+            cx="${random(70, 90)}" 
+            cy="${random(70, 90)}" 
+            fill="url('#${gradientID}')"
+            />
+         </svg>
+      `;
+   };
+   createEllips() {
+      const gradientID = random(0, 200);
+      const width = random(50, 250);
+      const height = random(50, 250);
+      return `
+         <svg width="${width}" height="${height}">
+            <defs>
+            <linearGradient id="${gradientID}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+            </defs>
+            <ellipse 
+            rx="${random(60, 110)}" 
+            ry="${random(30, 100)}" 
+            cx="${random(60, 110)}" 
+            cy="${random(30, 100)}" 
+            fill="url('#${gradientID}')"
+            />
+         </svg>`;
+       
+   };
+   createLine() {
+      const gradientID = random(85, 100);
+      const width = random(50, 250);
+      const height = random(50, 250);
+
+      return `
+         <svg width="${width}" height="${height}">
+            <defs>
+            <linearGradient id="${gradientID}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+            </defs>
+            <line 
+            x1="${random(0, 70)}" 
+            y1="${random(20, 30)}" 
+            x2="${random(0, 120)}" 
+            y2="${random(60, 180)}" 
+            stroke="url('#${gradientID}')"
+            />
+         </svg>
+      `;
+   };
+   createPolygon() {
+      const gradientID = random(85, 100);
+      const width = random(50, 250);
+      const height = random(50, 250);
+      return `
+         <svg width="${width}" height="${height}">
+            <defs>
+            <linearGradient id="${gradientID}" gradientTransform="rotate(${random(0, 100)})">
+               <stop offset="${random(0, 15)}%"  stop-color="${this.randomColor()}"></stop>
+               <stop offset="${random(85, 100)}%" stop-color="${this.randomColor()}"></stop>
+            </linearGradient>
+            </defs>
+            <polygon points="
+            ${random(5,10)},${random(100,150)}
+            ${random(100,150)},${random(5,15)}
+            ${random(200,250)},${random(100,150)}"
+            fill="url('#${gradientID}')"
+            />
+         </svg>
+      `;
+   };
+   removeShape() {
+      document.querySelector('.random-shape').remove();
+   };
+
+   createRandomShape() {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'random-shape rotate';
+      wrapper.style.cssText = `
+         position: absolute; 
+         top: ${random(1, window.innerHeight - 250)}px;
+         left: ${random(0, window.innerWidth - 250)}px;
+      `
+      
+      switch (this.#typesOfShape[this.randomType()]) {
+         case 'rect':
+             wrapper.innerHTML = this.createRect();
+             return wrapper;
+         case 'circle': 
+            wrapper.innerHTML = this.createCircle();
+            return wrapper;
+         case 'ellipse':
+            wrapper.innerHTML = this.createEllips();
+            return wrapper;
+         case 'line':
+            wrapper.innerHTML = this.createLine();
+            return wrapper;
+         case 'polygon': 
+            wrapper.innerHTML = this.createPolygon();
+            return wrapper;
+         default: 
+            wrapper.innerHTML = this.createPolygon();
+            return wrapper
+         }
+   }
+   trigger() {
+      document.body.append(this.createRandomShape());
+      // setTimeout(() => this.removeShape(), 2000);
+   }
+}
